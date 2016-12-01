7587bd90a712db51b187355be6e68744aa139bef
Yitaek Hwang <yitaek@gmail.com>
2016-11-30 20:46:00 -0500

    initial commit

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,24 @@
-import { renderComponent , expect } from '../test_helper';
+// Testing that App shows 'React simple starter'
+
+import { renderComponent, expect } from '../test_helper';
+
+// also import the file/component you want to test
+
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+// Use describe to group together similar tests
+describe('App', () => {
+	let component;
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
+	beforeEach(() => {
+		component = renderComponent(App);
+	});
+	
+	// Use 'it' to test a sinle attribute of a target
+	it('shows a comment box', () => {
+		expect(component.find('.comment-box')).to.exist
+		//jquery selector to expect a css component
+	})
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
 });
+
